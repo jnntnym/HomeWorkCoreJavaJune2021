295d6bb0bc51246581ca626fff0a90b79f2895b3
Jannat ul Nayeem <jannatulnayeem@Jannats-Air.fios-router.home>
2021-07-05 01:07:18 -0400

    2nd HomeWork 07.05.2021 1:06PM

--- homeWork/HelloWorld.java
+++ homeWork/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Everyone!");
 		System.out.println("This first github commit");
+		System.out.println("This is 2nd github commit");
 	}
 
 }
